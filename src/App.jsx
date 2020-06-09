0f8f0f1f5754b0728bafcd931c1531a361717156
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-06-09 10:25:10 -0500

    home page finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Pages/home';
 
 class App extends React.Component {
-
-    componentDidMount() {
-        fetch("http://ghibliapi.herokuapp.com/films")
-            .then(res => res.json())
-            .then(json => { this.setState({ films: json }) })
-        fetch("https://ghibliapi.herokuapp.com/people")
-            .then(res => res.json())
-            .then(json => { this.setState({ characters: json }) })
-    }
-
     render() {
         return (
             <BrowserRouter>
+            <nav className="nav justify-content-center p-3 shadow">
+                <h6>Michelle's Studio Ghibli Page</h6>
+            </nav>
                 <Switch>
-                    <Route exact path="/" component={() => <h1>Home Page</h1>} />;
+                    <Route exact path="/" component={ Home } />;
                     <Route exact path="/films" component={() => <h1>Films</h1>} />;
                     <Route exact path="/films/:id" component={() => <h1>Film Details</h1>} />;
                     <Route exact path="/characters" component={() => <h1>Characters</h1>} />;
@@ -29,3 +23,12 @@ class App extends React.Component {
 }
 
 export default App;
+
+// componentDidMount() {
+//     fetch("http://ghibliapi.herokuapp.com/films")
+//         .then(res => res.json())
+//         .then(json => { this.setState({ films: json }) })
+//     fetch("https://ghibliapi.herokuapp.com/people")
+//         .then(res => res.json())
+//         .then(json => { this.setState({ characters: json }) })
+// }
